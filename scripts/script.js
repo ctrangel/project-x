8d807f7b957455571eac03a165dc5b51dfc580a4
ctrangel <ctrangel@pointpark.edu>
2023-02-25 15:55:46 -0500

    Music file functionality
    added some js functions to play and pause different genres

--- scripts/script.js
+++ scripts/script.js
@@ -39,8 +39,75 @@ function displayDetails() {
   // console.log(row);
 }
 
+// Play rock song button function
 
-function scrolley() {
-  let bckSize = document.getElementsByTagName("body").style.backgroundSize;
-  console.log(bckSize);
+var rockAudio;
+function playRockSong() {
+  let randomArray = [
+    "../media/sound/songs/rock/come_together_Beatles.wav",
+    "../media/sound/songs/rock/dear_maria_count_me_in_All_Tim.wav",
+    "../media/sound/songs/rock/simple_man_lynard_skynard.wav",
+  ];
+
+  let button = document.getElementById("rock-btn");
+
+  if (!rockAudio) {
+    let randomSong =
+      randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(randomSong);
+    rockAudio = new Audio();
+    rockAudio.src = randomSong;
+  }
+
+  if (rockAudio.paused) {
+    rockAudio.play();
+    button.innerHTML = "Pause";
+  } else {
+    rockAudio.pause();
+    button.innerHTML = "Rock";
+  }
+  //display current song file below
+  let songNameElement = document.getElementById("song-name");
+
+  rockAudio.addEventListener("play", function () {
+    let songName = rockAudio.src.split("/").pop();
+    songNameElement.textContent = songName;
+  });
 }
+
+// Play pop song button function
+
+var popAudio;
+function playPopSong() {
+  let randomArray = [
+    "../media/sound/songs/pop/yourockmyworld-michaeljackson.wav",
+    "../media/sound/songs/pop/big_girls_don't_cry_Fergie.wav",
+  ];
+
+  let button = document.getElementById("pop-btn");
+
+  if (!popAudio) {
+    let randomSong =
+      randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(randomSong);
+    popAudio = new Audio();
+    popAudio.src = randomSong;
+  }
+
+  if (popAudio.paused) {
+    popAudio.play();
+    button.innerHTML = "Pause";
+  } else {
+    popAudio.pause();
+    button.innerHTML = "Pop";
+  }
+  //display current song file below
+  let songNameElement = document.getElementById("song-name");
+
+  popAudio.addEventListener("play", function () {
+    let songName = popAudio.src.split("/").pop();
+    songNameElement.textContent = songName;
+  });
+}
+
+
